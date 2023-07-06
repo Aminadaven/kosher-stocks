ecfec24c228d4fbf101a871ad21f7038d3675270
Aminadaven <aminadaven@gmail.com>
2023-07-06 23:02:35 +0300

    try to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+            // base: dev ? '' : process.env.BASE_PATH,
+            base: process.env.BASE_PATH,
         }
 	}
 };
