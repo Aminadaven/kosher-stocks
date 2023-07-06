cb0ce4340330f2cee82f75cd890532a5b1a97ebb
Aminadaven <aminadaven@gmail.com>
2023-07-06 23:25:40 +0300

    gpt

--- src/routes/[id]/+page.js
+++ src/routes/[id]/+page.js
@@ -1,9 +1,11 @@
 import data from '$lib/data/data.json';
 
-export const entries = (() => {
-    return [
-        Object.keys(data.index).map(key => {id: key})
-        // { slug: 'hello-world' },
-        // { slug: 'another-blog-post' }
-    ];
-})
+export const entries = Object.keys(data.index).map((key) => ({ id: key }));
+
+// export const entries = (() => {
+//     return [
+//         Object.keys(data.index).map(key => {id: key})
+//         // { slug: 'hello-world' },
+//         // { slug: 'another-blog-post' }
+//     ];
+// })
