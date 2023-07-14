724de19319f602deb9b7184ab29cb87e4d4bef44
Aminadaven <aminadaven@gmail.com>
2023-07-14 13:19:54 +0300

    fixed a bug, added collapse element and all new raw data

---
+++ src/components/collapse.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let header;
+</script>
+
+<div class="collapse collapse-arrow border border-base-300 bg-base-200">
+    <input type="checkbox" /> 
+    <div class="collapse-title text-xl font-medium">
+        {header}
+    </div>
+    <div class="collapse-content"> 
+        <slot />
+    </div>
+  </div>
+  
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	import dataJson from '$lib/data/data.json';
 	import { page } from '$app/stores';
-	import Accordion from '../../components/accordion.svelte';
 	import Meta from '../../components/meta.svelte';
+	import Collapse from '../../components/collapse.svelte';
 
 	const id = $page.params.id;
 	const stockRow = dataJson.data[dataJson.index[id]];
@@ -12,45 +12,84 @@
 <Meta title={meta} desc={meta} />
 
 <h1 class="my-5 p-2 text-4xl style4 inline-block">דף חברה {stockRow.stock.NameHeb}</h1>
-<div class="w-fit mx-auto style3">
-<!-- <Accordion header="מידע כללי"> -->
-	<h3>מידע כללי</h3>
-	<p>
-		סמל מניה: <strong>{stockRow.stock['SymbolHeb']}</strong> | <strong>{stockRow.stock['SymbolEng']}</strong>
-		<br />
-		מספר חברה (ח.פ.): {stockRow.stock['CorporateNo']}
-		<br />
-		סקטור-על: {stockRow.stock['SuperSectorHeb']} סקטור: {stockRow.stock['SectorHeb']} תת-סקטור: {stockRow.stock['SubSectorHeb']}
-		<!-- <br />
-		מדדים: {stockRow.stock['IndicesListHeb']?.map(index => index.Name)?.join(', ') ?? "החברה לא נכללת באף מדד"} -->
-	</p>
-	<br />
-<!-- </Accordion> -->
-{#if stockRow.permits}
-	<!-- <Accordion header="היתרי העסקה בשבת"> -->
-		{#each stockRow.permits as permit}
-		<h3>היתר העסקה בשבת</h3>
+<div class="mx-auto">
+	<Collapse header="מידע כללי">
 		<p>
-			תקופה: {permit.startDate} - {permit.endDate} | 
-			עילה: {permit.cause} {permit.subCause} | 
-			עובדים: {permit.employeesSum} כוננים: {permit.onCallEmployeesSum}
+			סמל מניה: <strong>{stockRow.stock['SymbolHeb']}</strong> |
+			<strong>{stockRow.stock['SymbolEng']}</strong>
+			<br />
+			מספר חברה (ח.פ.): {stockRow.stock['CorporateNo']}
+			<br />
+			סקטור-על: {stockRow.stock['SuperSectorHeb']} סקטור: {stockRow.stock['SectorHeb']} תת-סקטור: {stockRow
+				.stock['SubSectorHeb']}
 		</p>
+	</Collapse>
+	<br />
+
+	{#if stockRow.companyDetails}
+		<Collapse header="פרטי חברה">
+			<p>
+				{JSON.stringify(stockRow.companyDetails)}
+			</p>
+		</Collapse>
 		<br />
-		{/each}
-	<!-- </Accordion> -->
-{/if}
-{#if stockRow.approvals}
-	<!-- <Accordion header="היתרי עסקה"> -->
-		{#each stockRow.approvals as approval}
-		<h3>היתר עסקה {approval.type}</h3>
-		<span>
-			פירוט: {approval.details}
-		</span>
-		<span>
-			הערות: {approval.comments ?? 'אין'}
-		</span>
+	{/if}
+	{#if stockRow.shareHolders}
+		<Collapse header="מחזיקי מניה">
+			<p>
+				{JSON.stringify(stockRow.shareHolders)}
+			</p></Collapse
+		>
 		<br />
-		{/each}
-	<!-- </Accordion> -->
-{/if}
+	{/if}
+	{#if stockRow.financeData}
+		<Collapse header="מידע פיננסי">
+			<p>
+				{JSON.stringify(stockRow.financeData, null, 4)}
+			</p></Collapse
+		>
+		<br />
+	{/if}
+	{#if stockRow.reports}
+		<Collapse header="דיווחים">
+			<p>
+				{JSON.stringify(stockRow.reports)}
+			</p></Collapse
+		>
+		<br />
+	{/if}
+	{#if stockRow.tradeData}
+		<Collapse header="מידע מסחר">
+			<p>
+				{JSON.stringify(stockRow.tradeData)}
+			</p></Collapse
+		>
+		<br />
+	{/if}
+	{#if stockRow.permits}
+		<Collapse header="היתרי העסקה בשבת">
+			{#each stockRow.permits as permit}
+				<h3>היתר העסקה בשבת</h3>
+				<p>
+					תקופה: {permit.startDate} - {permit.endDate} | עילה: {permit.cause}
+					{permit.subCause} | עובדים: {permit.employeesSum} כוננים: {permit.onCallEmployeesSum}
+				</p>
+				<br />
+			{/each}
+		</Collapse>
+	{/if}
+	{#if stockRow.approvals}
+		<Collapse header="היתרי עסקה">
+			{#each stockRow.approvals as approval}
+				<h3>היתר עסקה {approval.type}</h3>
+				<span>
+					פירוט: {approval.details}
+				</span>
+				<span>
+					הערות: {approval.comments ?? 'אין'}
+				</span>
+				<br />
+			{/each}</Collapse
+		>
+	{/if}
 </div>
