f73669954694275979b0fa00bddb418a3cfda2c6
Aminadaven <aminadaven@gmail.com>
2023-09-03 00:09:11 +0300

    togglefilter and config

--- src/components/toggle-filter.svelte
+++ src/components/toggle-filter.svelte
@@ -1,9 +1,22 @@
 <script>
+	import { Toggle } from 'flowbite-svelte';
 	export let label, short;
 </script>
 
-<label class="label cursor-pointer">
+<div class="hidden lg:flex mx-4">
+	<Toggle on:change>
+		{label}
+	</Toggle>
+</div>
+
+<div class="lg:hidden mx-2">
+	<Toggle size="small" on:change>
+		{short}
+	</Toggle>
+</div>
+
+<!-- <label class="label cursor-pointer">
 	<span class="hidden lg:flex label-text mx-4">{label}</span>
 	<span class="lg:hidden label-text mx-2">{short}</span>
 	<input type="checkbox" class="toggle" on:change />
-</label>
+</label> -->
